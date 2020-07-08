15574ed1cd413e26a4e54d5d64ac041c9e1687bb
brittanyFitabase <brittany@fitabase.com>
2020-07-08 15:12:27 -0700

    Remove unneeded functionality

--- src/Engine.js
+++ src/Engine.js
@@ -1,33 +1,16 @@
-import { validateConditionFields, validatePredicates } from "./validation";
 import applicableActions from "./applicableActions";
-import { isDevelopment, isObject, toArray, toError } from "./utils";
+import {  toArray } from "./utils";
 
-const validate = schema => {
-  let isSchemaDefined = schema !== undefined && schema !== null;
-  if (isDevelopment() && isSchemaDefined) {
-    if (!isObject(schema)) {
-      toError(`Expected valid schema object, but got - ${schema}`);
-    }
-    return rule => {
-      validatePredicates([rule.conditions], schema);
-      validateConditionFields([rule.conditions], schema);
-    };
-  } else {
-    return () => {};
-  }
-};
 
 class Engine {
-  constructor(rules, schema) {
+  constructor(rules) {
     this.rules = [];
-    this.validate = validate(schema);
 
     if (rules) {
       toArray(rules).forEach(rule => this.addRule(rule));
     }
   }
   addRule = rule => {
-    this.validate(rule);
     this.rules.push(rule);
   };
   run = formData => Promise.resolve(applicableActions(this.rules, formData));
--- src/utils.js
+++ src/utils.js
@@ -33,49 +33,5 @@ export function toError(message) {
   }
 }
 
-export function isRefArray(field, schema) {
-  return (
-    schema.properties[field] &&
-    schema.properties[field].type === "array" &&
-    schema.properties[field].items &&
-    schema.properties[field].items["$ref"]
-  );
-}
-
-function fetchSchema(ref, schema) {
-  if (ref.startsWith("#/")) {
-    return ref
-      .substr(2)
-      .split("/")
-      .reduce((schema, field) => schema[field], schema);
-  } else {
-    toError(
-      "Only local references supported at this point use json-schema-deref"
-    );
-    return undefined;
-  }
-}
-
-export function extractRefSchema(field, schema) {
-  let { properties } = schema;
-  if (!properties || !properties[field]) {
-    toError(`${field} not defined in properties`);
-    return undefined;
-  } else if (properties[field].type === "array") {
-    if (isRefArray(field, schema)) {
-      return fetchSchema(properties[field].items["$ref"], schema);
-    } else {
-      return properties[field].items;
-    }
-  } else if (properties[field] && properties[field]["$ref"]) {
-    return fetchSchema(properties[field]["$ref"], schema);
-  } else if (properties[field] && properties[field].type === "object") {
-    return properties[field];
-  } else {
-    toError(`${field} has no $ref field ref schema extraction is impossible`);
-    return undefined;
-  }
-}
-
 const concat = (x, y) => x.concat(y);
 export const flatMap = (xs, f) => xs.map(f).reduce(concat, []);
